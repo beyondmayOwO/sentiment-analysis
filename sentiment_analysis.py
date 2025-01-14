a823e024c68ae3cf66d3bb0a235ae964f76c8d48
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2025-01-14 16:47:54 +0630

    Environment yaml file and the function for analyzing

---
+++ sentiment_analysis.py
@@ -0,0 +1,11 @@
+import requests
+
+def sentiment_analyzer(text_to_analyse):
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    myobj = {"raw_document": {"text": text_to_analyse}}
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    response = requests.post(url, json = myobj, headers = header)
+    print(f"Status Code: {response.status_code}")
+    # return response.text
+
+sentiment_analyzer("I love this technology")
