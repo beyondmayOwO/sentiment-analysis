4cf2dbdcd4acfb6edc4a4f27e287f5458817fbf1
beyondmayOwO <78870914+beyondmayOwO@users.noreply.github.com>
2025-01-17 15:11:10 +0630

    Added comments to the client-side handling

--- server.py
+++ server.py
@@ -2,10 +2,10 @@
     analysis to be executed over the Flask channel and deployed on
     localhost:5000.
 '''
-# Import Flask, render_template, request from the flask pramework package : TODO
-# Import the sentiment_analyzer function from the package created: TODO
+from flask import Flask, render_template, request
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
-#Initiate the flask app : TODO
+app = Flask("Sentiment Analyzer")
 
 @app.route("/sentimentAnalyzer")
 def sent_analyzer():
@@ -17,12 +17,13 @@ def sent_analyzer():
     # TODO
 
 @app.route("/")
-def render_index_page():
-    ''' This function initiates the rendering of the main application
-        page over the Flask channel
-    '''
+# def render_index_page():
+#     ''' This function initiates the rendering of the main application
+#         page over the Flask channel
+#     '''
     #TODO
+# def main():
+#     return "Hello"
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''#TODO
+    app.run(host="0.0.0.0", port=5000)
--- static/mywebscript.js
+++ static/mywebscript.js
@@ -1,12 +1,20 @@
 let RunSentimentAnalysis = ()=>{
     textToAnalyze = document.getElementById("textToAnalyze").value;
 
+    // Create a new XMLHttpRequest object
     let xhttp = new XMLHttpRequest();
+
+    // Define a callback function to handle the server response
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
+            // Update the HTML element with ID "system_response" with the server response
             document.getElementById("system_response").innerHTML = xhttp.responseText;
         }
     };
+    
+    // Open a GET request to the server endpoint "sentimentAnalyzer" with the user input as a query parameter
     xhttp.open("GET", "sentimentAnalyzer?textToAnalyze"+"="+textToAnalyze, true);
+    
+    // Send the request
     xhttp.send();
 }
